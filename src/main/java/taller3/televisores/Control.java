ff7b3fedb751da9457093f7eaafd22e806a32bc2
Andrea Amaya <44179880+kaamayam@users.noreply.github.com>
2021-03-16 21:45:10 -0500

    cambios

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,51 @@
+package taller3.televisores;
+
+public class Control {
+	TV tv;
+	
+	
+	
+	public TV getTv() {
+		return tv;
+	}
+
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+
+	public void turnOn() {
+		 tv.turnOn();
+	}
+	
+	public void turnOff() {
+		 tv.turnOff();
+	}
+	
+	public void canalUp() {
+		 tv.canalUp();
+	}
+	
+	public void canalDown() {
+		 tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		 tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		 tv.volumenDown();
+	}
+	
+	
+	public void setCanal(int canal_my) {
+		tv.setCanal(canal_my);
+	}
+	
+	public void enlazar(TV un_tv) {
+		this.tv = un_tv;
+		un_tv.setControl(this);
+	}
+	
+	
+}
